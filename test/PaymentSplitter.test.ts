013274c69c37a574a68c156defa8e118ad63ba5d
Unknown <me@rjchow.com>
2021-08-13 00:19:02 +0800

    Step 6: added test cases for _addPayees

--- test/PaymentSplitter.test.ts
+++ test/PaymentSplitter.test.ts
@@ -70,4 +70,46 @@ describe('PaymentSplitter', () => {
       )
     ).to.be.revertedWith('PaymentSplitter: payees and shares length mismatch');
   });
+
+  it('should fail to deploy if there is a payee with zero address', async () => {
+    const [deployer, account1] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    // deploy the payment splitter with a zero address payee
+    await expect(
+      PaymentSplitterFactory.connect(deployer).deploy(
+        [account1.address, ethers.constants.AddressZero],
+        [1, 2]
+      )
+    ).to.be.revertedWith('PaymentSplitter: account is the zero address');
+  });
+
+  it('should fail to deploy if there is a zero in the shares array', async () => {
+    const [deployer, account1, account2] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    // deploy the payment splitter with 0 in the shares array
+    await expect(
+      PaymentSplitterFactory.connect(deployer).deploy(
+        [account1.address, account2.address],
+        [1, 0]
+      )
+    ).to.be.revertedWith('PaymentSplitter: shares are 0');
+  });
+
+  it('should fail to deploy if there is a repeated payee', async () => {
+    const [deployer, account1] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    // deploy the payment splitter with two account1
+    await expect(
+      PaymentSplitterFactory.connect(deployer).deploy(
+        [account1.address, account1.address],
+        [1, 1]
+      )
+    ).to.be.revertedWith('PaymentSplitter: account already has shares');
+  });
 });
