db1b5db6c2001e6d7564b6ae185a1dc671dd1b69
Unknown <me@rjchow.com>
2021-08-13 07:43:29 +0800

    Step 8: added scenarios for release()

--- test/PaymentSplitter.test.ts
+++ test/PaymentSplitter.test.ts
@@ -135,4 +135,217 @@ describe('PaymentSplitter', () => {
     );
     expect(ethers.utils.formatEther(balance.toString())).to.equal('1.0');
   });
+
+  it('should reject a request to release funds to address without shares', async () => {
+    const [deployer, account1, account2] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    const paymentSplitterInstance = await PaymentSplitterFactory.connect(
+      deployer
+    ).deploy([account1.address], [1]);
+
+    await paymentSplitterInstance.deployed();
+
+    await expect(
+      paymentSplitterInstance.release(account2.address)
+    ).to.be.revertedWith('PaymentSplitter: account has no shares');
+  });
+
+  it('should reject a request to release funds to address not owed payment', async () => {
+    const [deployer, account1] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    const paymentSplitterInstance = await PaymentSplitterFactory.connect(
+      deployer
+    ).deploy([account1.address], [1]);
+
+    await paymentSplitterInstance.deployed();
+
+    await expect(
+      paymentSplitterInstance.release(account1.address)
+    ).to.be.revertedWith('PaymentSplitter: account is not due payment');
+  });
+
+  it('should distribute funds correctly to equal share holders', async () => {
+    const [deployer, account1, account2] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    // deploy the payment splitter with 10 shares total, 5 to each user
+    const paymentSplitterInstance = await PaymentSplitterFactory.connect(
+      deployer
+    ).deploy([account1.address, account2.address], [5, 5]);
+
+    await paymentSplitterInstance.deployed();
+
+    expect(paymentSplitterInstance.address).to.not.be.null;
+
+    // send 1 eth to the splitter
+    await deployer.sendTransaction({
+      to: paymentSplitterInstance.address,
+      value: ethers.utils.parseEther('1'),
+    });
+
+    // check initial balance so that we can calculate the transferred amount
+    const account1InitialBalance = await ethers.provider.getBalance(
+      account1.address
+    );
+
+    // release funds for account1
+    const account1Release = await paymentSplitterInstance.release(
+      account1.address
+    );
+
+    const account1NewBalance = await ethers.provider.getBalance(
+      account1.address
+    );
+    const account1Profit = account1NewBalance.sub(account1InitialBalance);
+    expect(ethers.utils.formatEther(account1Profit.toString())).to.equal('0.5');
+
+    // do the same for account 2
+    const account2InitialBalance = await ethers.provider.getBalance(
+      account2.address
+    );
+
+    // release funds for account1
+    const account2Release = await paymentSplitterInstance.release(
+      account2.address
+    );
+
+    const account2NewBalance = await ethers.provider.getBalance(
+      account2.address
+    );
+    const account2Profit = account2NewBalance.sub(account2InitialBalance);
+    expect(ethers.utils.formatEther(account2Profit.toString())).to.equal('0.5');
+  });
+
+  it('should distribute funds correctly to unequal share holders', async () => {
+    const [deployer, account1, account2] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    // deploy the payment splitter with 10 shares total, 5 to each user
+    const paymentSplitterInstance = await PaymentSplitterFactory.connect(
+      deployer
+    ).deploy([account1.address, account2.address], [1, 4]);
+
+    await paymentSplitterInstance.deployed();
+
+    expect(paymentSplitterInstance.address).to.not.be.null;
+
+    // send 1 eth to the splitter
+    await deployer.sendTransaction({
+      to: paymentSplitterInstance.address,
+      value: ethers.utils.parseEther('1'),
+    });
+
+    // check initial balance so that we can calculate the transferred amount
+    const account1InitialBalance = await ethers.provider.getBalance(
+      account1.address
+    );
+
+    // release funds for account1
+    const account1Release = await paymentSplitterInstance.release(
+      account1.address
+    );
+
+    const account1NewBalance = await ethers.provider.getBalance(
+      account1.address
+    );
+    const account1Profit = account1NewBalance.sub(account1InitialBalance);
+    expect(ethers.utils.formatEther(account1Profit.toString())).to.equal('0.2');
+
+    // do the same for account 2
+    const account2InitialBalance = await ethers.provider.getBalance(
+      account2.address
+    );
+
+    // release funds for account2
+    const account2Release = await paymentSplitterInstance.release(
+      account2.address
+    );
+
+    const account2NewBalance = await ethers.provider.getBalance(
+      account2.address
+    );
+    const account2Profit = account2NewBalance.sub(account2InitialBalance);
+    expect(ethers.utils.formatEther(account2Profit.toString())).to.equal('0.8');
+  });
+
+  it('should distribute funds correctly to equal share holders', async () => {
+    const [deployer, account1, account2] = await ethers.getSigners();
+    const PaymentSplitterFactory = await ethers.getContractFactory(
+      'PaymentSplitter'
+    );
+    // deploy the payment splitter with 10 shares total, 5 to each user
+    const paymentSplitterInstance = await PaymentSplitterFactory.connect(
+      deployer
+    ).deploy([account1.address, account2.address], [5, 5]);
+
+    await paymentSplitterInstance.deployed();
+
+    expect(paymentSplitterInstance.address).to.not.be.null;
+
+    // send 1 eth to the splitter
+    await deployer.sendTransaction({
+      to: paymentSplitterInstance.address,
+      value: ethers.utils.parseEther('1'),
+    });
+
+    // check initial balance so that we can calculate the transferred amount
+    const account1InitialBalance = await ethers.provider.getBalance(
+      account1.address
+    );
+
+    // release funds for account1
+    const account1Release = await paymentSplitterInstance.release(
+      account1.address
+    );
+
+    const account1NewBalance = await ethers.provider.getBalance(
+      account1.address
+    );
+    const account1Profit = account1NewBalance.sub(account1InitialBalance);
+    expect(ethers.utils.formatEther(account1Profit.toString())).to.equal('0.5');
+
+    // send another 1 eth to the splitter
+    await deployer.sendTransaction({
+      to: paymentSplitterInstance.address,
+      value: ethers.utils.parseEther('1'),
+    });
+
+    // at this point the total received by the splitter should be 2 eth
+    // therefore we should expect the account2 profit to be 1 eth
+    // and we should be able to withdraw another 0.5 eth for account1
+
+    const account2InitialBalance = await ethers.provider.getBalance(
+      account2.address
+    );
+
+    // release funds for account2
+    const account2Release = await paymentSplitterInstance.release(
+      account2.address
+    );
+
+    const account2NewBalance = await ethers.provider.getBalance(
+      account2.address
+    );
+    const account2Profit = account2NewBalance.sub(account2InitialBalance);
+    expect(ethers.utils.formatEther(account2Profit.toString())).to.equal('1.0');
+
+    // attempt second release for account1
+    const account1Release2 = await paymentSplitterInstance.release(
+      account1.address
+    );
+
+    const account1NewBalance2 = await ethers.provider.getBalance(
+      account1.address
+    );
+    const account1Profit2 = account1NewBalance2.sub(account1NewBalance);
+    expect(ethers.utils.formatEther(account1Profit2.toString())).to.equal(
+      '0.5'
+    );
+  });
 });
